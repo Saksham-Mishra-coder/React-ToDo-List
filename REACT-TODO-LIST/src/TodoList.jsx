241372efa803b0da813141d3d3d14cb16813d6dc
Saksham Mishra <sakshammishra2023@gmail.com>
2025-01-26 10:34:27 +0530

    Delete and Done task

--- REACT-TODO-LIST/src/TodoList.jsx
+++ REACT-TODO-LIST/src/TodoList.jsx
@@ -1,17 +1,53 @@
 import { useState } from "react";
-
+import { v4 as uuidv4 } from 'uuid';
 export default function TodoList() {
-    let [todo , setTodo] = useState([]);
-    let [newTask , setNewTask]= useState("");
+    let [todo , setTodo] = useState([{task: "sample-task", id: uuidv4(),isDone: false}]);//implement uuid here:)
 
+    let [newTask , setNewTask]= useState("");
     let newTodo = () =>{
-        setTodo([...todo, newTask]);
+        setTodo ((prevTodos) =>{
+                return [...prevTodos, {task: newTask, id: uuidv4(),isDone: false}]
+        });
         setNewTask("");
     };
 
     let updateTodo = (event) =>{
-        event.default
+       
         setNewTask(event.target.value);
+    };
+    let deleteTodo =(id)=> {
+     setTodo( (prevTodos)=>  todo.filter((prevTodos)=> prevTodos.id !=id));
+        console.log(id);
+    };
+    let doneOne = (id)=>{
+        setTodo((prevTodos)=>
+            prevTodos.map((todo) => {
+                if(todo.id==id){
+                    return{
+                        ...todo,
+                        isDone: true,
+                    }
+                }else {
+                    return todo;
+            }
+            })
+        );
+
+            
+    
+    
+    };
+    let doneAll = ()=>{
+        setTodo((prevTodos)=>
+            prevTodos.map(function (todo) {
+                return {
+                    ...todo,
+                    isDone: true,
+                };
+            }
+
+            )
+        );
     };
 
 
@@ -29,9 +65,16 @@ export default function TodoList() {
             <ul>
                 {
                     todo.map((todo) => (
-                    <li>{todo}</li>
+                    <li key = {todo.id}>
+                        <span style={todo.isDone ? {textDecorationLine: "line-through"}:{}}>{todo.task}</span>
+                        &nbsp;&nbsp;
+                        <button onClick={()=>{deleteTodo(todo.id)}}>delete</button>
+                        <button onClick={()=>{doneOne(todo.id)}}>Done</button>
+                        </li>
+                  
                     ))
-                }
+                }   <br></br><br></br>
+                          <button onClick={doneAll}   >Done All</button>
             </ul>
 
         </div>
